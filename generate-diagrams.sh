4c42970ac19cf09f8a2b52d661de002d461b50a2
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-07-17 14:06:24 +0200

    chore: improve script for generating diagrams

--- generate-diagrams.sh
+++ generate-diagrams.sh
@@ -3,7 +3,9 @@
 DIAGRAMS_DIR=./assets/puml
 IMAGES_DIR=./assets/images
 
+wget https://github.com/plantuml/plantuml/releases/download/v1.2023.10/plantuml-1.2023.10.jar
+
 for DIAGRAM in ${DIAGRAMS_DIR}/* ; do
-    plantuml ${DIAGRAM}
+    java -jar ./plantuml-1.2023.10.jar ${DIAGRAM}
 done
 mv ${DIAGRAMS_DIR}/*.png ${IMAGES_DIR}
